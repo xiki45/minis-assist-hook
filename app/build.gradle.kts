e94f14dc9c677a778dc2790d92dd1bdb47d9b3f9
ouo <ouo@users.noreply.github.com>
2026-09-02 19:51:48 +0800

    Bump hook version to 2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.openminis.hook"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "2.1"
+        versionCode = 4
+        versionName = "2.2"
     }
 
     buildTypes {
